137dae9b41163792e48f0150f2ddd36d3469bb08
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-11 08:14:42 +0500

    Added sign up button in signup screen

--- app/src/main/java/com/example/loopwell/ui/screens/auth/SignupScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/auth/SignupScreen.kt
@@ -1,12 +1,16 @@
 package com.example.loopwell.ui.screens.auth
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -19,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,7 +39,7 @@ fun SignupScreen() {
             .padding(20.dp)
     ) {
         Text(
-            text = "SIGNUP",
+            text = "SIGN UP",
             style = TextStyle(
                 textAlign = TextAlign.Center,
                 fontSize = 40.sp,
@@ -131,10 +136,30 @@ fun SignupScreen() {
             ),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 15.dp)
+                .padding(bottom = 30.dp)
                 .border(width = 1.5.dp, color = Color(0xFFE5E8EB)),
             shape = RoundedCornerShape(10.dp),
             placeholder = { Text("Enter your password") },
         )
+
+        Button(
+            onClick = {  },
+            shape = RoundedCornerShape(10.dp),
+            contentPadding = PaddingValues(vertical = 15.dp),
+            border = BorderStroke(1.5.dp, Color(0xFF4F7CCB)),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color(0xFF4F7CCB),
+                containerColor = Color.Transparent
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "SIGN UP",
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.W500
+                )
+            )
+        }
     }
 }
