f00b82bb1f2b3393acce2b842c109fd9a46f55c8
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-11 08:05:52 +0500

    Added signup heading in signup screen

--- app/src/main/java/com/example/loopwell/ui/screens/auth/SignupScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/auth/SignupScreen.kt
@@ -1,8 +1,36 @@
 package com.example.loopwell.ui.screens.auth
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SignupScreen() {
-
+    Column (
+        modifier = Modifier
+//            .fillMaxHeight()
+//            .fillMaxWidth()
+            .padding(20.dp),
+//        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "SIGNUP",
+            style = TextStyle(
+                textAlign = TextAlign.Center,
+                fontSize = 40.sp,
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 20.dp)
+        )
+    }
 }
