5ad606a82ea636a3ba38216195a9c84780111d73
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-11 07:49:57 +0500

    Add email title and textinput in login screen

--- app/src/main/java/com/example/loopwell/ui/screens/auth/LoginScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/auth/LoginScreen.kt
@@ -1,27 +1,42 @@
 package com.example.loopwell.ui.screens.auth
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen() {
+    var searchText by remember { mutableStateOf("") }
+
     Column (
         modifier = Modifier
             .fillMaxHeight()
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+            .fillMaxWidth()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = "LOGIN",
@@ -30,7 +45,39 @@ fun LoginScreen() {
                 fontSize = 40.sp,
             ),
             modifier = Modifier
-                .padding(10.dp)
+                .fillMaxWidth()
+                .padding(bottom = 20.dp)
+        )
+
+        Text(
+            text = "Email",
+            style = TextStyle(
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp,
+            ),
+            modifier = Modifier.padding(bottom = 5.dp)
+        )
+
+        TextField(
+            textStyle = TextStyle(
+                fontSize = 16.sp,
+            ),
+            value = searchText,
+            onValueChange = {
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.Transparent,
+                textColor = Color.Gray,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                placeholderColor = Color.Gray
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 15.dp)
+                .border(width = 1.5.dp, color = Color(0xFFE5E8EB)),
+            shape = RoundedCornerShape(10.dp),
+            placeholder = { Text("Enter your email") },
         )
     }
 }
