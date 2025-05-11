28abedd65b14767efcb7fd88655092f40e196320
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-11 07:29:47 +0500

    Add app name in splash screen

--- app/src/main/java/com/example/loopwell/ui/screens/auth/SplashScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/auth/SplashScreen.kt
@@ -4,13 +4,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 
 @Composable
@@ -20,13 +23,23 @@ fun SplashScreen() {
             .fillMaxHeight()
             .fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         AsyncImage(
             model = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/480px-Google_%22G%22_logo.svg.png",
             contentDescription = null,
             modifier = Modifier
                 .width(180.dp),
+        )
+
+        Text(
+            text = "LoopWell",
+            style = TextStyle(
+                textAlign = TextAlign.Center,
+                fontSize = 40.sp,
+            ),
+            modifier = Modifier
+                .padding(10.dp)
         )
     }
 }
