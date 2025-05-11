238b50023dc06ff3617e250be328f86f1dc0c504
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-11 07:39:00 +0500

    Add login heading in login screen

--- app/src/main/java/com/example/loopwell/ui/screens/auth/LoginScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/auth/LoginScreen.kt
@@ -1,8 +1,36 @@
 package com.example.loopwell.ui.screens.auth
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginScreen() {
-
+    Column (
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "LOGIN",
+            style = TextStyle(
+                textAlign = TextAlign.Center,
+                fontSize = 40.sp,
+            ),
+            modifier = Modifier
+                .padding(10.dp)
+        )
+    }
 }
