201c78ad98e9dbc1b243efceaed162109030ce6c
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-11 07:21:12 +0500

    Add logo in splash screen

--- app/src/main/java/com/example/loopwell/ui/screens/auth/SplashScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/auth/SplashScreen.kt
@@ -1,11 +1,32 @@
 package com.example.loopwell.ui.screens.auth
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 fun SplashScreen() {
-    Column {
-
+    Column (
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AsyncImage(
+            model = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/480px-Google_%22G%22_logo.svg.png",
+            contentDescription = null,
+            modifier = Modifier
+                .width(180.dp),
+        )
     }
 }
