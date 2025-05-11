88ca19a37a77e9c17d0cc2a236b5aab456570d16
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-11 07:34:07 +0500

    Started working on login screen

--- app/src/main/java/com/example/loopwell/MainActivity.kt
+++ app/src/main/java/com/example/loopwell/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.loopwell.ui.screens.auth.LoginScreen
 import com.example.loopwell.ui.screens.auth.SplashScreen
 import com.example.loopwell.ui.theme.LoopWellTheme
 
@@ -17,7 +18,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SplashScreen()
+            LoginScreen()
         }
     }
 }
---
+++ app/src/main/java/com/example/loopwell/ui/screens/auth/LoginScreen.kt
@@ -0,0 +1,8 @@
+package com.example.loopwell.ui.screens.auth
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen() {
+
+}
