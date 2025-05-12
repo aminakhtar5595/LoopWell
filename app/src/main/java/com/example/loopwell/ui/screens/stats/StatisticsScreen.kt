9e4204a6db74118361b766cd94e1ac90d6930f70
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-12 07:44:56 +0500

    Added completion rate box with title and description in statistics screen

--- app/src/main/java/com/example/loopwell/ui/screens/stats/StatisticsScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/stats/StatisticsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.loopwell.ui.screens.stats
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
@@ -14,6 +16,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
@@ -39,6 +42,22 @@ fun StatisticsScreen() {
                 text = "STATISTICS",
                 style = MaterialTheme.typography.headlineLarge.copy(textAlign = TextAlign.Center),
                 modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        Column (
+            modifier = Modifier
+                .border(width = 0.5.dp, color = Color.Black, shape = RoundedCornerShape(5))
+                .padding(10.dp)
+        ) {
+            Text(
+                text = "Completion Rate",
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Text(
+                text = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.",
+                style = MaterialTheme.typography.titleSmall
             )
         }
     }
