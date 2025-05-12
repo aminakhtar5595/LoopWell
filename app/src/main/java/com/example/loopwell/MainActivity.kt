61919f5198f69673f6dead9ad5e80b041731bde7
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-12 07:19:45 +0500

    Start working on add habit screen

--- app/src/main/java/com/example/loopwell/MainActivity.kt
+++ app/src/main/java/com/example/loopwell/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.loopwell.ui.screens.auth.SplashScreen
+import com.example.loopwell.ui.screens.habit.AddHabitScreen
 import com.example.loopwell.ui.screens.home.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+            AddHabitScreen()
         }
     }
 }
