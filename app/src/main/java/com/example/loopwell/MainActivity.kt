cffb7df29fb21e90dd4effcce1adcbfa1833a632
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-12 06:58:21 +0500

    Start working on home screen

--- app/src/main/java/com/example/loopwell/MainActivity.kt
+++ app/src/main/java/com/example/loopwell/MainActivity.kt
@@ -3,15 +3,14 @@ package com.example.loopwell
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.loopwell.ui.screens.auth.LoginScreen
-import com.example.loopwell.ui.screens.auth.SignupScreen
 import com.example.loopwell.ui.screens.auth.SplashScreen
+import com.example.loopwell.ui.screens.home.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SignupScreen()
+            HomeScreen()
         }
     }
 }
---
+++ app/src/main/java/com/example/loopwell/ui/screens/home/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.example.loopwell.ui.screens.home
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+
+}
