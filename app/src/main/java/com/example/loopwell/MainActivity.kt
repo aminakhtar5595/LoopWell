510600fe6831418237c6f8bb7585824222b18eeb
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-12 07:36:29 +0500

    Started working on statistics screen

--- app/src/main/java/com/example/loopwell/MainActivity.kt
+++ app/src/main/java/com/example/loopwell/MainActivity.kt
@@ -6,12 +6,13 @@ import androidx.activity.compose.setContent
 import com.example.loopwell.ui.screens.auth.SplashScreen
 import com.example.loopwell.ui.screens.habit.AddHabitScreen
 import com.example.loopwell.ui.screens.home.HomeScreen
+import com.example.loopwell.ui.screens.stats.StatisticsScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AddHabitScreen()
+            StatisticsScreen()
         }
     }
 }
---
+++ app/src/main/java/com/example/loopwell/ui/screens/stats/StatisticsScreen.kt
@@ -0,0 +1,8 @@
+package com.example.loopwell.ui.screens.stats
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StatisticsScreen() {
+
+}
