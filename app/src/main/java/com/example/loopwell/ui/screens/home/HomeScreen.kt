d584e94f8bd5a3514fc8d9e37c336c560144f0a0
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-12 07:17:37 +0500

    Add add habit button on home screen

--- app/src/main/java/com/example/loopwell/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/home/HomeScreen.kt
@@ -1,20 +1,26 @@
 package com.example.loopwell.ui.screens.home
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
@@ -86,5 +92,21 @@ fun HomeScreen() {
         )
         Divider()
 
+        Button(
+            onClick = {  },
+            shape = RoundedCornerShape(10.dp),
+            contentPadding = PaddingValues(vertical = 15.dp),
+            border = BorderStroke(1.5.dp, Color(0xFF4F7CCB)),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color(0xFF4F7CCB),
+                containerColor = Color.Transparent
+            ),
+            modifier = Modifier.fillMaxWidth().padding(top = 30.dp)
+        ) {
+            Text(
+                text = "ADD HABIT",
+                style = MaterialTheme.typography.titleLarge,
+            )
+        }
     }
 }
