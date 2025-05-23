e91a6810adb7c7c11eb803643fa00126b7c8ed58
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-23 08:20:07 +0500

    Updated screen background color in splash

--- app/src/main/java/com/example/loopwell/ui/screens/auth/SplashScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/auth/SplashScreen.kt
@@ -1,27 +1,23 @@
 package com.example.loopwell.ui.screens.auth
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import com.example.loopwell.R
 
 @Composable
 fun SplashScreen() {
     Column (
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .background(color = Color(0xFF1D1D1D)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
