a05f0b18ed1e49b0d767c31e8cf2db22ecd958cf
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-23 08:12:17 +0500

    Updated splash icon

--- app/src/main/java/com/example/loopwell/MainActivity.kt
+++ app/src/main/java/com/example/loopwell/MainActivity.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.loopwell.ui.screens.auth.SplashScreen
-import com.example.loopwell.ui.screens.habit.AddHabitScreen
-import com.example.loopwell.ui.screens.home.HomeScreen
-import com.example.loopwell.ui.screens.stats.StatisticsScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            StatisticsScreen()
+            SplashScreen()
         }
     }
 }
--- app/src/main/java/com/example/loopwell/ui/screens/auth/SplashScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/auth/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.example.loopwell.ui.screens.auth
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,9 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.example.loopwell.R
 
 @Composable
 fun SplashScreen() {
@@ -22,18 +25,7 @@ fun SplashScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        AsyncImage(
-            model = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/480px-Google_%22G%22_logo.svg.png",
-            contentDescription = "App logo",
-            modifier = Modifier
-                .width(180.dp),
-        )
-
-        Text(
-            text = "LoopWell",
-            style = MaterialTheme.typography.headlineLarge.copy(textAlign = TextAlign.Center),
-            modifier = Modifier
-                .padding(10.dp)
-        )
+        Image(painter = painterResource(id = R.drawable.splash_icon),
+            contentDescription = "Splash Icon")
     }
 }
