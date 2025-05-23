d111f93504ae39e793c5496fd789747d8d858ce8
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-05-23 09:55:35 +0500

    Implemented UI for onboarding screen 4

--- app/src/main/java/com/example/loopwell/ui/screens/auth/OnboardingScreenFour.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/auth/OnboardingScreenFour.kt
@@ -1,8 +1,48 @@
 package com.example.loopwell.ui.screens.auth
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.loopwell.R
 
 @Composable
 fun OnboardingScreenFour() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF121212))
+            .padding(horizontal = 10.dp)
+        ,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(painter = painterResource(id = R.drawable.onboarding_icon_four),
+            contentDescription = "Onboarding Icon", Modifier.size(220.dp))
 
+        Text(
+            text = "Stay Motivated",
+            style = MaterialTheme.typography.headlineSmall.copy(color = Color(0xFFa21752), fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(top = 30.dp, bottom = 10.dp)
+        )
+
+        Text(
+            text = "Create streaks of success for your habits and complete all your tasks. Use the charts and tools to deeply analyze your progress.",
+            style = MaterialTheme.typography.bodyLarge.copy(color = Color.White, textAlign = TextAlign.Center, fontSize = 18.sp),
+        )
+    }
 }
