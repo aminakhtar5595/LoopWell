f9140437e494e0d01f38d680bece6061420ceff5
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-06-01 23:43:47 +0500

    Remove extra imports on home screen

--- app/src/main/java/com/example/loopwell/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/home/HomeScreen.kt
@@ -1,28 +1,5 @@
 package com.example.loopwell.ui.screens.home
-
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen() {
