f593217159d2a4f7e7e125b92882312c8ba5da3f
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-06-15 10:14:34 +0500

    Implemented calendar UI, added logic for current and other days

--- app/src/main/java/com/example/loopwell/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/home/HomeScreen.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -37,10 +39,9 @@ import java.time.LocalDate
 fun HomeScreen() {
     var selectedDate by remember { mutableStateOf(LocalDate.now()) }
 
-    // Generate a week of dates centered around today
-    val days = remember {
-        (0..14).map { LocalDate.now().minusDays(7).plusDays(it.toLong()) }
-    }
+    val today = remember { LocalDate.now() }
+    val startDate = today.minusDays(2)
+    val days = (0..14).map { startDate.plusDays(it.toLong()) }
 
     Column (
         modifier = Modifier
@@ -49,20 +50,19 @@ fun HomeScreen() {
             .padding(horizontal = 10.dp, vertical = 15.dp),
     ) {
         Row (
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
                 .padding(bottom = 20.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Column {
-                Text(
-                    text = "Today",
-                    style = MaterialTheme.typography.headlineSmall.copy(color = Color.White, fontWeight = FontWeight.Bold),
-                )
-            }
+            Text(
+                text = "Today",
+                style = MaterialTheme.typography.headlineSmall.copy(color = Color.White, fontWeight = FontWeight.Bold),
+            )
 
             Image(painter = painterResource(id = R.drawable.add_icon),
-                contentDescription = "Onboarding Icon", Modifier.size(35.dp))
+                contentDescription = "Add habit icon", Modifier.size(35.dp))
         }
 
         HorizontalCalendar(
@@ -80,29 +80,45 @@ fun HorizontalCalendar(
     selectedDate: LocalDate,
     onDateSelected: (LocalDate) -> Unit
 ) {
-    LazyRow(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
+    LazyRow {
         items(days) { day ->
             val isSelected = day == selectedDate
 
             Box(
                 modifier = Modifier
                     .clickable { onDateSelected(day) }
-                    .padding(end = 20.dp)
+                    .padding(end = 8.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(if (isSelected) Color(0xFFc03755) else Color(0xFF1c1c1c)),
             ) {
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = day.dayOfWeek.name.take(3), style = MaterialTheme.typography.bodyMedium.copy(color = Color.White), modifier = Modifier.padding(bottom = 20.dp))
-
-                    Text(text = day.dayOfMonth.toString(),
-                        style = MaterialTheme.typography.titleLarge.copy(color = Color.White, fontWeight = FontWeight.Bold),
+                    Text(text = day.dayOfWeek.name.take(3),
+                        style = MaterialTheme.typography.labelMedium.copy(color = Color.White),
                         modifier = Modifier
-                            .clip(RoundedCornerShape(30.dp))
-                            .background(if (isSelected) Color(0xFF6200EE) else Color.Transparent)
-                            .padding(vertical = 5.dp, horizontal = 10.dp)
+                            .padding(vertical = 8.dp),
+                    )
+
+                    Box(
+                        modifier = Modifier
+                            .width(48.dp)
+                            .background(if (isSelected) Color(0xFF912941) else Color(0xFF242424))
+                            .padding(vertical = 5.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = day.dayOfMonth.toString(),
+                            style = MaterialTheme.typography.titleLarge.copy(color = Color.White)
                         )
+                    }
+
+                    if (isSelected) {
+                        Box(
+                            modifier = Modifier
+                                .width(48.dp)
+                                .height(2.dp)
+                                .background(Color(0xFFc0939e))
+                        )
+                    }
                 }
             }
         }
