58a3d66f3d45a6a01f565bb60808f8e3fa04661b
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-07-20 09:45:24 +0500

    Start working on categories screen

--- app/src/main/java/com/example/loopwell/ui/screens/categories/CategoriesScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/categories/CategoriesScreen.kt
@@ -1,8 +1,32 @@
 package com.example.loopwell.ui.screens.categories
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.KeyboardArrowLeft
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.loopwell.ui.theme.BackgroundColor
+import com.example.loopwell.ui.theme.Red
 
 @Composable
 fun CategoriesScreen() {
-
+    Column (
+        modifier = Modifier.fillMaxSize().background(color = BackgroundColor).padding(vertical = 15.dp, horizontal = 10.dp)
+    ) {
+        Row {
+            Icon(
+                imageVector = Icons.Outlined.KeyboardArrowLeft,
+                contentDescription = "Search Icon",
+                modifier = Modifier.size(30.dp),
+                tint = Red
+            )
+        }
+    }
 }
