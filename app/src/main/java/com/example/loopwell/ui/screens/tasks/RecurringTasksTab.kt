4205b7918a7622895e6a1cd6cd05b3b1a5493a3d
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-07-24 10:33:30 +0500

    Implemented tabs for single and recurring tasks in tasks screen

---
+++ app/src/main/java/com/example/loopwell/ui/screens/tasks/RecurringTasksTab.kt
@@ -0,0 +1,10 @@
+package com.example.loopwell.ui.screens.tasks
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RecurringTasksTab() {
+    Text("Completed tasks go here", color = MaterialTheme.colorScheme.onBackground)
+}
