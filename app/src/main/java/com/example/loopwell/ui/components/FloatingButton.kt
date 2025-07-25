5df4ba82d311828bc144601dbda048695877b6f2
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-07-25 10:54:02 +0500

    Implemented UI of create habit, tasks, recurring task bottom sheet

--- app/src/main/java/com/example/loopwell/ui/components/FloatingButton.kt
+++ app/src/main/java/com/example/loopwell/ui/components/FloatingButton.kt
@@ -1,23 +1,19 @@
 package com.example.loopwell.ui.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.outlined.DateRange
 import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.KeyboardArrowRight
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -36,12 +32,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.loopwell.R
-import com.example.loopwell.ui.screens.categories.ModalInfo
 import com.example.loopwell.ui.theme.BackgroundColor
 import com.example.loopwell.ui.theme.BorderGray
 import com.example.loopwell.ui.theme.Red
@@ -92,47 +86,57 @@ fun BottomSheetExample(
                     .fillMaxWidth()
                     .background(BackgroundColor)
             ) {
-                Row (
-                    modifier = Modifier.padding(15.dp).fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Row (
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(15.dp)
-                    ) {
-                        Text(
-                            text = "●",
-                            style = MaterialTheme.typography.headlineMedium.copy(color = Red),
-                        )
-                        Text(
-                            text = "Create category",
-                            style = MaterialTheme.typography.titleLarge.copy(color = Color.White, fontSize = 18.sp, fontWeight = FontWeight.W500)
-                        )
-                    }
-                    Image(
-                        painter = painterResource(id = R.drawable.other_icon),
-                        contentDescription = "Category Icon",
-                        Modifier.size(50.dp)
-                    )
-                }
-                HorizontalDivider(thickness = 1.dp, color = BorderGray)
-                ModalInfo(icon = Icons.Outlined.Edit, title = "Category name")
-                ModalInfo(icon = Icons.Outlined.DateRange, title = "Category icon")
-                ModalInfo(icon = Icons.Outlined.Settings, title = "Category color")
-                Spacer(modifier = Modifier.height(10.dp))
-                Button(
-                    onClick = { },
-                    shape = RoundedCornerShape(10.dp),
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    colors = ButtonDefaults.buttonColors(contentColor = Red, containerColor = Color.Transparent),
-                ) {
-                    Text("CREATE CATEGORY",
-                        style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold, fontSize = 18.sp)
-                    )
-                }
+                ModalInfo(icon = Icons.Outlined.Edit, title = "Habit", description = "Activity that repeats over time. It has detailed tracking and statistics.")
+                ModalInfo(icon = Icons.Outlined.DateRange, title = "Recurring task", description = "Activity that repeats over time without tracking or statistics.")
+                ModalInfo(icon = Icons.Outlined.Settings, title = "Task", description = "Single instance activity without tracking over time.")
             }
         }
+    }
+}
+
+@Composable
+fun ModalInfo(icon: ImageVector, title: String, description: String) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = title,
+                modifier = Modifier.size(27.dp),
+                tint = Red
+            )
+            Spacer(modifier = Modifier.width(15.dp))
+
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleLarge.copy(
+                        color = Color.White,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.W500
+                    )
+                )
+                Text(
+                    text = description,
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        color = Color.LightGray
+                    )
+                )
+            }
+
+            Icon(
+                imageVector = Icons.Outlined.KeyboardArrowRight,
+                contentDescription = title,
+                modifier = Modifier.size(25.dp),
+                tint = Color.Gray
+            )
+        }
+        HorizontalDivider(thickness = 1.dp, color = BorderGray)
     }
 }
