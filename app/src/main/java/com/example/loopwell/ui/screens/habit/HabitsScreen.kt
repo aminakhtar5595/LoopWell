c009b57d7dbf5f15debc9e9a982704cc34a7bdb0
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-07-27 09:37:13 +0500

    Start working on habit screen

--- app/src/main/java/com/example/loopwell/ui/screens/habit/HabitsScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/screens/habit/HabitsScreen.kt
@@ -1,8 +1,41 @@
 package com.example.loopwell.ui.screens.habit
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.loopwell.ui.components.FloatingButton
+import com.example.loopwell.ui.components.Header
+import com.example.loopwell.ui.theme.BackgroundColor
 
 @Composable
-fun HabitsScreen() {
+fun HabitsScreen(navController: NavController) {
+    Box (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = BackgroundColor)
+    ) {
+        Column (
+            modifier = Modifier
+                .padding(horizontal = 10.dp, vertical = 15.dp)
+        ) {
+            Header()
+        }
 
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(20.dp)
+        ) {
+            FloatingButton(onClick = {
+                navController.navigate("expense")
+            })
+        }
+    }
 }
