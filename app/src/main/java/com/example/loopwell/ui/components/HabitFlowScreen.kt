788545128554f11ecf0e159384b80d5696d0df10
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-07-27 08:59:27 +0500

    Implemented vertical grid for displaying category info columns wise in create habit flow

--- app/src/main/java/com/example/loopwell/ui/components/HabitFlowScreen.kt
+++ app/src/main/java/com/example/loopwell/ui/components/HabitFlowScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,10 +25,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.loopwell.R
+import com.example.loopwell.ui.model.Category
 
 @Composable
 fun HabitFlowScreen(
@@ -37,6 +41,24 @@ fun HabitFlowScreen(
     leftLabel: String,
     rightLabel: String
 ) {
+    val categories = listOf(
+        "Quit a bad habit" to R.drawable.bad_habit_icon,
+        "Arts" to R.drawable.art_icon,
+        "Meditation" to R.drawable.meditation_icon,
+        "Study" to R.drawable.study_icon,
+        "Sports" to R.drawable.sports_icon,
+        "Entertainment" to R.drawable.entertainment_icon,
+        "Social" to R.drawable.social_icon,
+        "Finance" to R.drawable.finance_icon,
+        "Health" to R.drawable.health_icon,
+        "Work" to R.drawable.work_icon,
+        "Nutrition" to R.drawable.nutrition_icon,
+        "Home" to R.drawable.home_icon,
+        "Tasks" to R.drawable.task_icon,
+        "Outdoor" to R.drawable.outdoor_icon,
+        "Other" to R.drawable.other_icon,
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -46,7 +68,6 @@ fun HabitFlowScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(top = 30.dp)
         ) {
             Text(
@@ -58,45 +79,20 @@ fun HabitFlowScreen(
             )
             Spacer(modifier = Modifier.height(30.dp))
 
-            Row {
-                Text(
-                    text = "Art",
-                    style = MaterialTheme.typography.headlineSmall.copy(
-                        color = Color(0xFFa21752)
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                items(categories.size) { index ->
+                    val category = categories[index]
+                    CategoryInfo(
+                        title = category.first,
+                        icon = category.second,
                     )
-                )
-
-                Image(
-                    painter = painterResource(id = R.drawable.art_icon),
-                    contentDescription = "Onboarding Image",
-                    modifier = Modifier.size(220.dp)
-                )
+                }
             }
-
-//            Image(
-//                painter = painterResource(id = imageRes),
-//                contentDescription = "Onboarding Image",
-//                modifier = Modifier.size(220.dp)
-//            )
-
-//            Text(
-//                text = title,
-//                style = MaterialTheme.typography.headlineSmall.copy(
-//                    color = Color(0xFFa21752),
-//                    fontWeight = FontWeight.Bold
-//                ),
-//                modifier = Modifier.padding(top = 30.dp, bottom = 10.dp)
-//            )
-
-//            Text(
-//                text = description,
-//                style = MaterialTheme.typography.bodyLarge.copy(
-//                    color = Color.White,
-//                    textAlign = TextAlign.Center,
-//                    fontSize = 18.sp
-//                ),
-//                modifier = Modifier.padding(horizontal = 20.dp)
-//            )
         }
 
         Row(
@@ -119,5 +115,31 @@ fun HabitFlowScreen(
                 modifier = Modifier.clickable { onNextClick() }
             )
         }
+    }
+}
+
+@Composable
+fun CategoryInfo(title: String, icon: Int, modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .background(
+                color = Color.Gray.copy(alpha = 0.1f),
+                shape = RoundedCornerShape(5.dp)
+            )
+            .padding(horizontal = 10.dp, vertical = 5.dp)
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleMedium.copy(
+                color = Color.White
+            ),
+            modifier = Modifier.weight(1f) // Ensures text takes up remaining space
+        )
+        Image(
+            painter = painterResource(id = icon),
+            contentDescription = "Art icon",
+            modifier = Modifier.size(40.dp)
+        )
     }
 }
