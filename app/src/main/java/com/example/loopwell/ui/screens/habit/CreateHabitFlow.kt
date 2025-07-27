795f0dd1d4e4345d0924ab08af5f7ba9b9a0a87c
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-07-27 08:09:43 +0500

    Start working on create habit flow

---
+++ app/src/main/java/com/example/loopwell/ui/screens/habit/CreateHabitFlow.kt
@@ -0,0 +1,59 @@
+package com.example.loopwell.ui.screens.habit
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavController
+import com.example.loopwell.R
+import com.example.loopwell.ui.components.HabitFlowScreen
+
+@Composable
+fun CreateHabitFlow(navController: NavController) {
+    var currentPage by remember { mutableStateOf(0) }
+
+    val onboardingItems = listOf(
+        Triple(R.drawable.onboarding_icon_one, "Select a category for your habit", "This app will help you to keep an organized routine as you build new habits!"),
+        Triple(R.drawable.onboarding_icon_two, "Build a Better Routine", "To begin using LoopWell, start by recording the habits you want to track in your life along with your pending tasks."),
+//        Triple(R.drawable.onboarding_icon_three, "Make Each Day Count", "Each day, you will receive a list of all your scheduled activities. Make sure of our customizable reminders to ensure you complete them all!"),
+//        Triple(R.drawable.onboarding_icon_four, "Stay Motivated", "Create streaks of success for your habits and complete all your tasks. Use the charts and tools to deeply analyze your progress."),
+//        Triple(R.drawable.onboarding_icon_five, "Customize Your Journey", "Try all the customization options, create a daily journal and protect your data. There are many features to discover!"),
+    )
+
+    val isLastPage = currentPage == onboardingItems.lastIndex
+    val isFirstPage = currentPage == 0
+//    val item = onboardingItems[currentPage]
+
+    val (image, title, desc) = onboardingItems[currentPage]
+
+    HabitFlowScreen(
+        imageRes = image,
+        title = title,
+        description = desc,
+        currentPage = currentPage,
+        totalPages = onboardingItems.size,
+
+        onSkipClick = {
+            if (isFirstPage) {
+                navController.navigate("home") {
+                    popUpTo("onboarding") { inclusive = true }
+                }
+            } else {
+                currentPage--
+            }
+        },
+        onNextClick = {
+            if (isLastPage) {
+                navController.navigate("home") {
+                    popUpTo("onboarding") { inclusive = true }
+                }
+            } else {
+                currentPage++
+            }
+        },
+
+        leftLabel = if (isFirstPage) "Skip" else "Back",
+        rightLabel = if (isLastPage) "Done" else "Next"
+    )
+}
